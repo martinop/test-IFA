ff6449f1f240f1daa73b597f73980191a3d8a9a8
Martin Ocando <martinocandop@gmail.com>
2018-12-08 22:09:46 -0400

    AppBar Component markup | FontAwesome imported

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+// FontAwesome Libray
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+// Components
+import AppBar from './Components/AppBar';
+
+library.add(fas, fab, far);
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <AppBar />
       </div>
     );
   }
---
+++ src/Components/AppBar/index.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import {
+  Badge,
+  Collapse,
+  Container,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './styles.css';
+import Avatar from './avatar.png';
+
+export default class Example extends React.Component {
+  state = { isOpen: false }
+  toggle = () =>
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }))
+  render() {
+    return (
+      <div>
+        <Navbar light expand="lg" className="appbar">
+          <Container>
+            <NavbarBrand href="/"><FontAwesomeIcon icon="square" /></NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="mr-auto mainmenu" navbar>
+                <NavItem>
+                  <NavLink href="/">Dashboard</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/">Network</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/">Network</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/">Network</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/">Network</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/">Network</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/">Billing</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/">Network</NavLink>
+                </NavItem>
+              </Nav>
+              <Nav className="ml-auto userarea" navbar>
+                <NavItem>
+                  <NavLink href="/"><FontAwesomeIcon icon="search" size="lg" /></NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/"><FontAwesomeIcon icon={['far', 'question-circle']} size="lg" /></NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/"><FontAwesomeIcon icon="bell" size="lg" /><Badge color="danger">4</Badge></NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav>
+                    <img src={Avatar} alt="Avatar" className="avatar" />
+                    <FontAwesomeIcon icon="chevron-down" />
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      My account
+                    </DropdownItem>
+                    <DropdownItem>
+                      Profile
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                      Logout
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
